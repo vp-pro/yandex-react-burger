6d5a0158d9e354e8a4e860fa23f4bfd9f3ce6613
Vladislav Panferov <panfyor26@gmail.com>
2023-10-28 17:09:04 +1100

    Multi webSocket

--- src/services/middlewares/wsMiddleware.ts
+++ src/services/middlewares/wsMiddleware.ts
@@ -1,40 +1,73 @@
 // websocketMiddleware.ts
-import { Middleware, MiddlewareAPI, Dispatch } from "redux";
-import { setOrders } from "../slices/ordersSlice"; // Import the action
+import { Middleware, MiddlewareAPI, Dispatch } from 'redux';
+import { setOrders } from '../slices/ordersSlice';
+import { setUserOrders } from '../slices/userOrdersSlice';
 
 interface WebSocketAction {
   type: string;
   payload: any;
 }
 
-let socket: WebSocket | null = null;
+const connections: Record<string, WebSocket | null> = {};
+
+const getAccessToken = () => {
+  // Extract the access token from localStorage
+  const token = localStorage.getItem('accessToken');
+  if (token) {
+    // Remove the 'Bearer ' prefix and any leading/trailing whitespace
+    return token.replace(/^Bearer\s+/i, '').trim();
+  }
+  return '';
+};
+
+const getWebSocketForEndpoint = (endpoint: string) => {
+  if (!connections[endpoint]) {
+    const accessToken = getAccessToken();
+    const url = `wss://norma.nomoreparties.space/${endpoint}?token=${accessToken}`;
+    connections[endpoint] = new WebSocket(url);
+
+    connections[endpoint].onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      if (data.success) {
+        if (endpoint === 'orders/all') {
+          if (data.orders) {
+            store.dispatch(setOrders(data.orders)); // Dispatch the "setOrders" action for ordersSlice
+          }
+        } else if (endpoint === 'orders') {
+          if (data.userOrders) {
+            store.dispatch(setUserOrders(data.userOrders)); // Dispatch the "setUserOrders" action for userOrdersSlice
+          }
+        }
+      }
+    };
+
+    connections[endpoint].onclose = () => {
+      connections[endpoint] = null;
+    };
+  }
+  return connections[endpoint];
+};
 
 const websocketMiddleware: Middleware = (store: MiddlewareAPI) => (next: Dispatch) => (
   action: WebSocketAction
 ) => {
   const { type, payload } = action;
 
-  if (type === "websocket/connect") {
-    if (socket) socket.close();
-    socket = new WebSocket(payload.url);
-
-    socket.onmessage = (event) => {
-      const data = JSON.parse(event.data);
-      store.dispatch({ type: "websocket/message", payload: data });
-    };
-
-    socket.onclose = () => {
-      store.dispatch({ type: "websocket/disconnect" });
-    };
-  } else if (type === "websocket/send" && socket) {
-    socket.send(JSON.stringify(payload));
-  } else if (type === "websocket/disconnect" && socket) {
-    socket.close();
-    socket = null;
-  } else if (type === "websocket/message") {
-    // Check if the payload contains "orders"
-    if (payload.success && payload.orders) {
-      store.dispatch(setOrders(payload.orders)); // Dispatch the "setOrders" action
+  if (type === 'websocket/connect') {
+    const endpoint = payload.endpoint;
+    getWebSocketForEndpoint(endpoint);
+  } else if (type === 'websocket/send') {
+    const { endpoint, data } = payload;
+    const socket = getWebSocketForEndpoint(endpoint);
+    if (socket) {
+      socket.send(JSON.stringify(data));
+    }
+  } else if (type === 'websocket/disconnect') {
+    const endpoint = payload.endpoint;
+    const socket = connections[endpoint];
+    if (socket) {
+      socket.close();
+      connections[endpoint] = null;
     }
   }
 
