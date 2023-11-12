d03aa63005438522d806f877658caa713a02eb19
Vladislav Panferov <panfyor26@gmail.com>
2023-11-12 12:14:50 +1100

    gh-pages

--- src/components/BurgerIngredients/BurgerIngredients.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import BurgerIngredients from './BurgerIngredients'
-
-describe('<BurgerIngredients />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<BurgerIngredients />)
-  })
-})
